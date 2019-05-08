9ef0aa4c7b2e13e9b4ac5414b45bd3ac0aa7963f
Samya <khemri.samya.isart@gmail.com>
2019-05-08 07:33:39 -0400

    db init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
+    ArticlesModule
+  ],
 })
 export class AppModule {}
